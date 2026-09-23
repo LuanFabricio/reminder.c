5eff99bd6a351779f9154a05a4116245b39bc2fb
Luan Fabricio <luanlimaleite@hotmail.com>
2026-09-23 14:15:05 -0300

    feat(log): adds color based on log label

--- include/log.h
+++ include/log.h
@@ -4,6 +4,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#define ANSI_COLOR_FG_BLUE 	"\x1b[34m"
+#define ANSI_COLOR_FG_RED 	"\x1b[31m"
+#define ANSI_COLOR_FG_YELLOW 	"\x1b[33m"
+#define ANSI_COLOR_BG_BLUE 	"\x1b[37;44m"
+#define ANSI_COLOR_BG_RED 	"\x1b[37;41m"
+#define ANSI_COLOR_BG_YELLOW 	"\x1b[37;43m"
+#define ANSI_COLOR_RESET 	"\x1b[0m"
+
 typedef enum {
 	LOG_LABEL_INFO,
 	LOG_LABEL_WARNING,
@@ -11,11 +19,23 @@ typedef enum {
 } Log_Label;
 
 const char* log_cstr_label(const Log_Label label);
+const char* log_cstr_label_fg_color(const Log_Label label);
 
 #define log_format(file, label, ...)\
 	do {\
-		fprintf(file, "[%s|%s:%s:%d]", log_cstr_label(label), __FILE__, __FUNCTION__, __LINE__);\
+		const char* FG_COLOR = log_cstr_label_fg_color(label);\
+		fprintf(\
+			file,\
+			"%s["ANSI_COLOR_RESET\
+			"%s"\
+			"%s|%s:%s:%d]",\
+			FG_COLOR,\
+			log_cstr_label(label),\
+			FG_COLOR,\
+			__FILE__, __FUNCTION__, __LINE__\
+		);\
 		fprintf(file, __VA_ARGS__);\
+		fprintf(file, ANSI_COLOR_RESET);\
 	} while(0)
 
 #define log_panic(...)\
--- src/log.c
+++ src/log.c
@@ -3,10 +3,21 @@
 const char* log_cstr_label(const Log_Label label)
 {
 	switch (label) {
-		case LOG_LABEL_INFO: return "INFO";
-		case LOG_LABEL_WARNING: return "WARNING";
-		case LOG_LABEL_ERROR: return "ERROR";
+		case LOG_LABEL_INFO: return ANSI_COLOR_BG_BLUE"INFO"ANSI_COLOR_RESET;
+		case LOG_LABEL_WARNING: return ANSI_COLOR_BG_YELLOW"WARNING"ANSI_COLOR_RESET;
+		case LOG_LABEL_ERROR: return ANSI_COLOR_BG_YELLOW"ERROR"ANSI_COLOR_RESET;
 	}
 
 	return "UNDEFINED";
+}
+
+const char* log_cstr_label_fg_color(const Log_Label label)
+{
+	switch (label) {
+		case LOG_LABEL_INFO: return ANSI_COLOR_FG_BLUE;
+		case LOG_LABEL_WARNING: return ANSI_COLOR_FG_YELLOW;
+		case LOG_LABEL_ERROR: return ANSI_COLOR_FG_RED;
+	}
+
+	return "";
 }
